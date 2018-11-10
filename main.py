6fd1f995fd71a9254fa66c17d0dc2d78e4f6be83
Satyam <satyamkumar2302@gmail.com>
2018-11-10 19:04:48 +0530

    needs optimization

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ mybooks = bookdb["allBooks"]
 rentedbooks = bookdb["rentedBooks"]
 
 
-button_rent = Button(main, text="Rent a book")
+button_rent = Button(main, text="Rent a book",command=lambda:rentbookfn())
 button_rent.pack(padx=25, pady=25)
 
-button_return = Button(main, text="Return a book")
+button_return = Button(main, text="Return a book",command=lambda:retbookfn())
 button_return.pack(padx=25, pady=25)
 
 button_addbook = Button(main, text="Add a book to Library", command=lambda:addbookfn())
@@ -40,7 +40,7 @@ def addbookfn():
     entry_isbn = Entry(smain)
     entry_isbn.grid(row=10, column=100)
 
-    button_submit = Button(smain, text="ADD", command=lambda:insertdb(entry_bname, entry_aname, entry_isbn))
+    button_submit = Button(smain, text="Add", command=lambda:insertdb(entry_bname, entry_aname, entry_isbn))
     button_submit.grid(row=20, column=10)
 
 def insertdb(ebname,eaname,eisbn):
@@ -73,6 +73,59 @@ def rmdb(rmisbn):
     isbn = rmisbn.get()
     myquery = {"ISBN":isbn}
     mybooks.delete_one(myquery)
+
+def rentbookfn():
+    smain = Tk()
+    smain.geometry("400x600")
+    lbox = Listbox(smain, width=150)
+    for x in mybooks.find({},{"_id":0, "Book Name":1,"Author Name":1, "ISBN":1}):
+        x = dict(x)
+        lbox.insert(1,x)
+    lbox.pack()
+
+    label_rent = Label(smain, text="Enter ISBN of book to rent")
+    label_rent.pack()
+
+    entry_rent = Entry(smain)
+    entry_rent.pack()
+
+    button_rent = Button(smain, text="Rent", command=lambda:rentbook(entry_rent))
+    button_rent.pack()
+
+def rentbook(erent):
+    rent = erent.get()
+    for x in mybooks.find():
+        if x["ISBN"]==rent:
+            rentedbooks.insert_one(x)
+            mybooks.delete_one(x)
+
+def retbookfn():
+    smain = Tk()
+    smain.geometry("400x600")
+    lbox = Listbox(smain, width=150)
+    for x in rentedbooks.find({},{"_id":0, "Book Name":1,"Author Name":1, "ISBN":1}):
+        x = dict(x)
+        lbox.insert(1,x)
+    lbox.pack()
+
+    label_ret = Label(smain, text="Enter ISBN of book to return")
+    label_ret.pack()
+
+    entry_ret = Entry(smain)
+    entry_ret.pack()
+
+    button_ret = Button(smain, text="Return", command=lambda:retbook(entry_ret))
+    button_ret.pack()
+
+def retbook(eret):
+    ret = eret.get()
+    for x in rentedbooks.find():
+        if x["ISBN"]==ret:
+            rentedbooks.delete_one(x)
+            mybooks.insert_one(x)
+    
+    
+    
 
         
     
